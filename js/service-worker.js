e5238bc5454fc58411d72fabfba20795e47f7661
AmineElMallouki12 <amine-el-mallouki@hotmail.fr>
2024-11-18 21:51:43 +0100

    Initial commit

---
+++ js/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'weather-app-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
